485bfbc3f40f208993f3318b753b0a62e2f3d87a
Janit Bidhan <janitbidhan@gmail.com>
2023-03-25 23:34:56 -0400

    Fixing Mongo Connection

--- src/main/java/tech/timelyx/backend/repository/CourseRepository.java
+++ src/main/java/tech/timelyx/backend/repository/CourseRepository.java
@@ -3,6 +3,7 @@ package tech.timelyx.backend.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import tech.timelyx.backend.model.Courses;
+
 @Repository
 public interface CourseRepository extends MongoRepository<Courses, String> {
 }
